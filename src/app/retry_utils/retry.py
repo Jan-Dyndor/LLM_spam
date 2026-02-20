e61cd01c6ccb19b350794041440de903b4e30d88
Jan Dyndor <jandyndor@gmail.com>
2026-02-20 20:27:32 +0100

    wokring retry on Google APi LLM + modification of exception to cover retry with tenacity

--- src/app/retry_utils/retry.py
+++ src/app/retry_utils/retry.py
@@ -1,12 +1,13 @@
 import requests
 
 from app.exceptions.exceptions import (
-    LLMError,
+    LLM_API_Error,
     LLMInvalidJSONError,
     LLMInvalidValidationError,
 )
 
-retryable_status_codes = [408, 429, 500, 502, 503, 504]
+# -1 is to retry when API somehow returned empty response to retry it
+retryable_status_codes = [-1, 408, 429, 500, 502, 503, 504]
 
 
 def should_retry(exception: BaseException) -> bool:
@@ -14,8 +15,11 @@ def should_retry(exception: BaseException) -> bool:
         return True
     elif isinstance(exception, requests.ConnectionError):
         return True
-    elif isinstance(exception, LLMError):
-        return True
+    elif isinstance(exception, LLM_API_Error):
+        if exception.api_status_code in retryable_status_codes:
+            return True
+        else:
+            return False
     elif isinstance(exception, LLMInvalidJSONError):
         return True
     elif isinstance(exception, LLMInvalidValidationError):
