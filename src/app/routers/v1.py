4c2fe5e6e82f34f7005d48b1e5ab6f1457df8cc7
Jan Dyndor <jandyndor@gmail.com>
2026-02-22 14:39:15 +0100

    first version of redis cache

--- src/app/routers/v1.py
+++ src/app/routers/v1.py
@@ -1,11 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
+from redis.asyncio import Redis
 
+from app.logging.logg import logger
 from app.schemas.pydantic_schemas import InputText, LLM_Response
 from app.services.spam_classification import classify_spam
 
 router = APIRouter(prefix="/v1", tags=["v1"])
 
 
+def get_reddis(request: Request) -> Redis:
+    return request.app.state.redis
+
+
 @router.post("/classify", response_model=LLM_Response)
-async def ask_llm(input: InputText):
-    return classify_spam(input.text)
+async def ask_llm(input: InputText, redis: Redis = Depends(get_reddis)):
+
+    value = await redis.get(input.text)
+    if value is not None:
+        logger.info("CACHE HIT")
+    else:
+        model_output = classify_spam(input.text)
+        model_output_json = model_output.model_dump_json()
+        await redis.set(input.text, model_output_json)
+        return model_output
+
+    return LLM_Response.model_validate_json(value)
