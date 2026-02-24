ba60b5d6a39cd4883a03ce85380713250546d881
Jan Dyndor <jandyndor@gmail.com>
2026-02-24 18:58:10 +0100

    set env to work no matter what current working dir is

--- src/app/config/settings.py
+++ src/app/config/settings.py
@@ -1,15 +1,15 @@
 from pydantic import Field, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
-import os
+from pathlib import Path
 
-DOTENV = os.path.join(os.path.dirname(__file__), ".env")
-print(DOTENV)
+
+root = Path(__file__).resolve().parents[3]
 
 
 class Settings(BaseSettings):
     google_api_key: SecretStr = Field(..., validation_alias="GEMINI_API_KEY")
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=root / ".env", env_file_encoding="utf-8")
 
 
 settings = Settings()  # type: ignore
