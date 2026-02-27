bde0f147c96423319ce6d737929c7d11721291a6
Jan Dyndor <jandyndor@gmail.com>
2026-02-27 16:43:34 +0100

    testing - first version of connection to db, two tables workign together. Later will be added JWT

--- src/app/routers/v1.py
+++ src/app/routers/v1.py
@@ -13,7 +13,7 @@ from app.services.spam_classification import classify_spam
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from app.db.database import get_db
-from app.db.db_models import User
+from app.db.db_models import User, Predictions
 
 router = APIRouter(prefix="/v1", tags=["v1"])
 
@@ -22,35 +22,68 @@ def get_reddis(request: Request) -> Redis:
     return request.app.state.redis
 
 
-@router.post("/classify", response_model=LLM_Response)
-async def ask_llm(input: InputText, redis: Redis = Depends(get_reddis)):
+# Temporary fix - passing user_id in request body.
+# Right now if user is in databese - so ealier created account and now his/her ID we assoscaite it with inforamtion in predictions table
+# And allow user to save its question about spam to LLM
+# If user is not in db his/her ask will not be saved
 
-    value = await redis.get(input.text)
+
+# Later we will do JWT authentication adn get rid of query_param user_id!! TODO
+@router.post("/classify", response_model=LLM_Response)
+async def ask_llm(
+    user_id: int,
+    input: InputText,
+    redis: Redis = Depends(get_reddis),
+    db: Session = Depends(get_db),
+):
+
+    value = await redis.get(input.text)  # string
     if value is not None:
         logger.info("CACHE HIT")
+        value = LLM_Response.model_validate_json(value)
     else:
-        model_output = classify_spam(input.text)
-        model_output_json = model_output.model_dump_json()
+        value = classify_spam(input.text)
+        model_output_json = value.model_dump_json()
         await redis.setex(input.text, 300, model_output_json)
-        return model_output
+        # value = LLM_Response.model_validate_json(model_output_json)
+        # return value
 
-    return LLM_Response.model_validate_json(value)
+    # model_output_validated = LLM_Response.model_validate_json(value)
+    result_user_id = db.execute(select(User).where(User.id == user_id))
+    user = result_user_id.scalars().first()
+    logger.warning("Linia 54")
+    if user:
+        logger.warning("MAM USERA")
+        new_prediction = Predictions(
+            user_id=user_id,
+            model_name="TEST",
+            input_text=input.text,
+            label=value.label,
+            confidence=value.confidence,
+            reason=value.reason,
+            prompt_version="v1",
+        )
+        db.add(new_prediction)
+        db.commit()
+        db.refresh(new_prediction)
+
+    return value
 
 
 @router.post("/users", response_model=UserResponse)
 async def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
-    result = db.execute(select(User).where(User.username == user.username))
+    result_username = db.execute(select(User).where(User.username == user.username))
 
-    existing_user = result.scalars().first()
+    existing_user = result_username.scalars().first()
     if existing_user:
-        raise HTTPException(status_code=400, detail="Username already exist")
+        raise HTTPException(status_code=404, detail="Username already exists")
 
-    result = db.execute(select(User).where(User.email == user.email))
+    result_email = db.execute(select(User).where(User.email == user.email))
 
-    existing_email = result.scalars().first()
+    existing_email = result_email.scalars().first()
     if existing_email:
-        raise HTTPException(status_code=400, detail="Email already exist")
+        raise HTTPException(status_code=404, detail="Email already exist")
 
     new_user = User(username=user.username, email=user.email)
     db.add(new_user)
