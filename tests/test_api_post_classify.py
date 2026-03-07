87b5a40db0cdfe9c48d394e8885ecb3f13d7576f
Jan Dyndor <jandyndor@gmail.com>
2026-03-07 13:43:00 +0100

    small change

--- tests/test_api_post_classify.py
+++ tests/test_api_post_classify.py
@@ -5,8 +5,7 @@ from sqlalchemy import select
 from app.db.db_models import Predictions, User
 from app.exceptions.exceptions import LLMInvalidJSONError, LLMInvalidValidationError
 
-# NO AUTH TEST - assume OK authorization with JWT
-# NO CACHE TESTS
+# NO AUTH TEST - assume OK authorization with JWT (mocked auth)
 
 
 def test_health_happy(client, health_happy_path):
@@ -27,6 +26,9 @@ def test_ask_llm_happy_no_auth(
     client,
     session_fixture,
 ):
+    """
+    Function tests happy path of endpoint /classify with correct user in DB, mock authentication and ssaving predictions to DB
+    """
     # Add User to DB
     new_user = User(
         id=1, username="test_user", email="test@email.com", password_hash="test"
@@ -104,7 +106,7 @@ def test_ask_llm_wrong_user_input_too_long(
 def test_ask_llm_wrong_llm_response_json(
     mock_llm, mock_verify_token, user_input, redis_fixture, client, session_fixture
 ):
-
+    """Function tests /classify endpoint with API LLM error and mocked auth"""
     mock_verify_token.return_value = "1"
     mock_llm.side_effect = LLMInvalidJSONError()
     redis_fixture.get.return_value = None
@@ -125,6 +127,8 @@ def test_ask_llm_wrong_llm_response_json(
 def test_ask_llm_wrong_llm_response_walidation(
     mock_llm, mock_verify_token, user_input, redis_fixture, client, session_fixture
 ):
+    """Function tests /classify endpoint with API LLM error nad mocked auth"""
+
     mock_verify_token.return_value = "1"
     mock_llm.side_effect = LLMInvalidValidationError()
     redis_fixture.get.return_value = None
@@ -151,13 +155,27 @@ def test_ask_llm_cache_hit(
     client,
     session_fixture,
 ):
+    """Function test /classify endpoint with mocked auth and redis cache hit, saving output to DB"""
+    new_user = User(
+        id=1, username="test_user", email="test@email.com", password_hash="test"
+    )
+
+    session_fixture.add(new_user)
+    session_fixture.commit()
+    session_fixture.refresh(new_user)
+
     mock_verify_token.return_value = "1"
     redis_fixture.get.return_value = Model_Response_Happy_REDIS_Response
 
     response = client.post("/v1/classify", json={"text": user_input})
 
-    data_obj = session_fixture.execute(select(Predictions))
-    data = data_obj.scalars().all()
+    data_obj = session_fixture.execute(
+        select(Predictions).where(Predictions.user_id == 1)
+    )
+    data = data_obj.scalars().first()
+
+    mock_llm.assert_not_called()  # classify_spam function not called
+    redis_fixture.setex.assert_not_awaited()
 
     assert response.status_code == 200
     assert response.json()["label"] == "spam"
@@ -166,8 +184,49 @@ def test_ask_llm_cache_hit(
         response.json()["reason"]
         == "Contains unsolicited promotion for Viagra, a common spam topic."
     )
+    assert data.model_name == "test_model"
 
-    mock_llm.assert_not_called()  # classify_spam function not called
-    redis_fixture.assert_not_awaited()
 
-    assert data == []
+@patch("app.routers.v1.verify_access_token")
+@patch("app.routers.v1.classify_spam")
+def test_ask_llm_no_user_in_db(
+    mock_llm,
+    mock_verify_token,
+    user_input,
+    client,
+    session_fixture,
+    redis_fixture,
+    Model_Response_Happy_JSON_Validated,
+    Model_Response_Happy_REDIS,
+):
+    """Function tests /classify endpoint with mocked auth and somehow now User who invoked endpoint in DB = no saving model output"""
+    users_obj = session_fixture.execute(select(User))
+    users = users_obj.scalars().all()
+    assert users == []
+
+    mock_verify_token.return_value = "1"
+    redis_fixture.get.return_value = None
+    mock_llm.return_value = Model_Response_Happy_JSON_Validated
+
+    response = client.post("/v1/classify", json={"text": user_input})
+
+    assert response.status_code == 200
+    assert response.json()["label"] == "spam"
+    redis_fixture.setex.assert_awaited_once_with(
+        user_input, 300, Model_Response_Happy_REDIS
+    )
+
+    predictions_obj = session_fixture.execute(select(Predictions))
+    preds = predictions_obj.scalars().all()
+    assert preds == []
+
+
+@patch("app.routers.v1.verify_access_token")
+def test_invalid_token(mock_verify_token, client, user_input):
+    """Function will test invalid token value - function verify_access_token returns None"""
+
+    mock_verify_token.return_value = None
+    response = client.post("v1/classify", json={"text": user_input})
+
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Invalid or expired token"
