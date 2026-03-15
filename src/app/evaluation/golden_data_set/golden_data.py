9b8dcc49cd930e5852891cbeee1d031140863294
Jan Dyndor <jandyndor@gmail.com>
2026-03-15 19:33:28 +0100

    correct mistake in naming and CICD

--- src/app/evaluation/golden_data_set/golden_data.py
+++ src/app/evaluation/golden_data_set/golden_data.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
-from app.config.settings import get_settings
 from app.schemas.pydantic_schemas import LLM_Response
 
-settings = get_settings()
-test_dataset_name = settings.ai_model.test_dataset_name
 
-test_dataset_name = [
+dataset_name = [
     {
         "text": "You have been selected for an exclusive investment opportunity. Act fast.",
         "label": "spam",
@@ -17,7 +14,7 @@ test_dataset_name = [
     },
 ]
 
-test_df = pd.DataFrame(test_dataset_name)
+test_df = pd.DataFrame(dataset_name)
 
 example_response = LLM_Response(label="spam", confidence=0.56, reason="Test")
 example_response2 = LLM_Response(label="spam", confidence=1, reason="222")
